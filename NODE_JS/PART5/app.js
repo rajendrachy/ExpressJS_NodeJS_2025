e31035ffda816a450e1a946614aa2fabf6210166
rajendrachy <chyrajendra32@gmail.com>
2025-07-22 17:13:32 +0530

    change nodejs to NODEJS

---
+++ NODE_JS/PART5/app.js
@@ -0,0 +1,13 @@
+// global.console.log('Subscribe'); // both are same
+// globalThis.console.log('Subscribe');
+
+
+
+
+
+
+console.log(globalThis.process);
+// node is all about your module and react is all about your component
+// globalThis.console.log(module);
+
+
---
+++ NODE_JS/PART6/MODULES/math.js
@@ -0,0 +1,9 @@
+// This math js file is a module
+
+const add = (a, b) => {
+  return a + b;
+}
+
+module.exports = add;
+
+
