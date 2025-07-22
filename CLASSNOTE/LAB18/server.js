b949343be5d5e2a719c7997d8ac382b115267ea2
rajendrachy <chyrajendra32@gmail.com>
2025-07-22 17:08:10 +0530

    lab18 file uploading and type of img file

---
+++ CLASSNOTE/LAB18/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const multer = require('multer')
+const app = express()
+
+app.use(express.static('uploads'))
+
+const upload = multer({dest: "./uploads"})
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/upload',upload.array("test-upload", 2), (req, res)=>{
+    console.log(req.files)
+    // res.send(<img src="/${req.file.filename}">)
+    res.redirect('/')
+})
+
+
+app.listen(3000, (req,res)=>{
+     console.log("Server Stsrted");
+})
+
+
