d907077f62a0ab0353f08c902a709bbb40a1d8cd
rajendrachy <chyrajendra32@gmail.com>
2025-08-03 23:18:59 +0530

    lab 5 rev complet

--- CLASSNOTE/LAB5/app.js
+++ CLASSNOTE/LAB5/app.js
@@ -1,65 +1,43 @@
-const http = require('http')
-const fs = require('fs')
-const querystring = require('querystring') // -> is used in Node.js to import the querystring module, which helps parse and stringify URL query strings
-
-
-
-// res.setHeader()	When you want to set headers gradually or conditionally
-// res.writeHead()	When you want to send headers and status code all at once
-
-
-//  What is a URL Encoder?
-// A URL encoder is a mechanism that converts text (like spaces, symbols, or special characters) into a safe format for URLs.
-
-// Input string:
-// Hello World!@#&
-// URL encoded:
-// Hello%20World%21%40%23%26
+// const http = require('http')
+// const fs = require('fs')
+// const querystring = require('querystring') // -> is used in Node.js to import the querystring module, which helps parse and stringify URL query strings
 
 
 
 
-
-
-
-
-
-const server = http.createServer((req, res)=>{
-    if(req.url == "/login" && req.method == "GET"){
-        fs.readFile('./login.html', (err, data)=>{
-            if(!err){
-                res.writeHead(200, {'content-type': 'text/html'})
-                res.write(data)
-                res.end()
+// const server = http.createServer((req, res)=>{
+//     if(req.url == "/login" && req.method == "GET"){
+//         fs.readFile('./login.html', (err, data)=>{
+//             if(!err){
+//                 res.writeHead(200, {'content-type': 'text/html'})
+//                 res.write(data)
+//                 res.end()
 
                
-            }
-        })
-    }else if(req.url == "/login" && req.method == "POST"){
-        let body = ""
-        req.on("data", (chunk)=>{   //'data' – triggered when data is received  'end' – triggered when all data has been received
+//             }
+//         })
+//     }else if(req.url == "/login" && req.method == "POST"){
+
+//         let body = ""
+//         req.on("data", (chunk)=>{   //'data' – triggered when data is receiveiving  'end' – triggered when all data has been received
 
 
-            body += chunk.toString() // .toString() converts the binary Buffer into a readable string.
+//             body += chunk.toString() // .toString() converts the binary Buffer into a readable string.
 
-
-        })
-        req.on('end', ()=> {
-            console.log(querystring.parse(body));
-        })
-    }
-})
-
-
-
-server.listen(3001, ()=>{
-    console.log('server started...');
-})
+//         })
+//         req.on('end', ()=> {
+//             console.log(querystring.parse(body));
+//         })
+//     }
+// })
 
 
 
 
 
+// server.listen(3000, ()=>{
+//     console.log('server started...');
+// })
 
 
 
@@ -76,8 +54,7 @@ server.listen(3001, ()=>{
 
 
 
-
-//-----another------------------
+//-----Another------------------
 // const http = require('http');
 // const fs = require('fs');
 // const querystring = require('querystring');
@@ -122,31 +99,4 @@ server.listen(3001, ()=>{
 
 
 
-
-
-// const http = require('http');
-// const fs = require('fs');
-
-// const server = http.createServer((req, res) => {
-//        if(req.url == '/') {
-//         res.setHeader("COntent-Type", "text/plane");
-//         res.write("Home Page");
-//         res.end();
-//        }   else if(req.url === '/login') {
-//         res.writeHead(200, {'Content-Type':'text/html'});
-//            fs.readFile('./login.html', (err, data) => {
-//             if(!err) {
-//                 res.write(data);
-//                 res.end();
-//             }
-//            })
-//        }
-
-// })
-
-
-// const PORT = 3000;
-// server.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// })
 
