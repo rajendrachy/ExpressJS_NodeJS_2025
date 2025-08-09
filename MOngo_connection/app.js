5c8dd92f4ce0fd6d339e198222cc65802553b1d2
rajendrachy <chyrajendra32@gmail.com>
2025-08-09 22:55:58 +0530

    mongodb conection

--- MOngo_connection/app.js
+++ MOngo_connection/app.js
@@ -1,11 +1,29 @@
+// Import the Express framework and create an Express application
 const app = require('express')();
+
+// Import Mongoose to connect and interact with MongoDB
 const mongoose = require('mongoose');
+
+// Import the User model (Mongoose schema) from the 'models' folder
 const User = require('./models/userMl');
+
+// Create a basic HTTP server using Node.js and attach the Express app to it
+// This is useful if you plan to use WebSocket libraries like Socket.IO
 const http = require('http').Server(app);
 
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://user:userss@cluster0.mryrym2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://user:userss@cluster0.mryrym2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Connection failed:", err.message);
+});
+
+
+
+
+
 
 // Once connected, add a user
 mongoose.connection.once('open', () => {
@@ -13,7 +31,8 @@ mongoose.connection.once('open', () => {
 
   const user = new User({
     name: "Alice",
-    email: "alice@example.com"
+    email: "alice@example.com",
+    id: 23666
   });
 
   user.save().then(result => {
@@ -21,9 +40,22 @@ mongoose.connection.once('open', () => {
   });
 });
 
+
+
+
+
 http.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+
+
+
+
+
+
+
+
 
 
 
--- MOngo_connection/models/userMl.js
+++ MOngo_connection/models/userMl.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String
+  email: String,
+  id: Number
 })
 
 
