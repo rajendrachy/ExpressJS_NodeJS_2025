2ade55ad7a02af5ea7e29ae8bc37b0f51ce65405
rajendrachy <chyrajendra32@gmail.com>
2025-08-09 22:39:48 +0530

    lab15 complete

--- CLASSNOTE/LAB15/app.js
+++ CLASSNOTE/LAB15/app.js
@@ -1,12 +1,29 @@
+// const express = require('express');
+// const app = express();
+// const productRoutes = require('./routes/products');
+
+// const PORT = process.env.PORT || 3000;
+
+// app.use('/products', productRoutes);
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+
+
+
+
 const express = require('express');
 const app = express();
-const productRoutes = require('./routes/products');
 
-const PORT = process.env.PORT || 3000;
+const proRouter = require('./routes/products');
 
-app.use('/products', productRoutes);
+app.use('/products', proRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+app.listen(3000, () => {
+    console.log("Server started");
+})
 
--- CLASSNOTE/LAB15/routes/products.js
+++ CLASSNOTE/LAB15/routes/products.js
@@ -1,25 +1,62 @@
+// const express = require('express');
+// const router = express.Router();
+// const products = require('../products.json'); // Load the JSON file
+
+// // or use, 
+// // // Read products.json file once at the top
+// // const products = JSON.parse(fs.readFileSync('products.json', 'utf-8'));
+
+
+// // GET /products/category/:categoryName
+// router.get('/category/:categoryName', (req, res) => {
+//     const category = req.params.categoryName.toLowerCase();
+//     const maxPrice = parseFloat(req.query.maxPrice);
+
+//     const result = products.filter(product => 
+//         product.category.toLowerCase() === category &&
+//         (!maxPrice || product.price <= maxPrice)
+//     );
+
+//     res.json(result);
+// });
+
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const product = require('../products.json');
+
 const router = express.Router();
-const products = require('../products.json'); // Load the JSON file
-
-// or use, 
-// // Read products.json file once at the top
-// const products = JSON.parse(fs.readFileSync('products.json', 'utf-8'));
 
 
-// GET /products/category/:categoryName
 router.get('/category/:categoryName', (req, res) => {
-    const category = req.params.categoryName.toLowerCase();
-    const maxPrice = parseFloat(req.query.maxPrice);
+   const cName = req.params.categoryName;
 
-    const result = products.filter(product => 
-        product.category.toLowerCase() === category &&
-        (!maxPrice || product.price <= maxPrice)
-    );
+   res.sendFile(path.join(__dirname, '../products.json'));
 
-    res.json(result);
-});
+
+    console.log(product);
+
+  product.filter((val) => {
+    if(val.category == cName) {
+       // console.log(val.name);
+       console.log(val.name + "-" + val.id);
+        //res.send(val.name + " " + val.id);
+    }
+  })
+})
+
+
 
 module.exports = router;
-
 
