a63126f7076e937ce766b830414f91b2b26c22d8
rajendrachy <chyrajendra32@gmail.com>
2025-08-11 00:03:13 +0530

    add correct version

--- CLASSNOTE/LAB11/app.js
+++ CLASSNOTE/LAB11/app.js
@@ -4,26 +4,36 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
+//app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 
-app.get('/about', (req, res) => {
-      res.sendFile(path.join(__dirname , './public/about.html'));
-})
+app.use(express.static(path.join(__dirname, 'public'), {extensions: ['html']}));
 
 
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/contact.html'));
-})
+// app.get('/about', (req, res) => {
+//       res.sendFile(path.join(__dirname , './public/about.html'));
+// })
 
-app.get('/services', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/services.html'));
-})
+
+// app.get('/contact', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/contact.html'));
+// })
+
+
+// app.get('/services', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/services.html'));
+// })
 
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Server start...");
 })
+
+
+
+
+
+
 
 
 
