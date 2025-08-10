308894dcb75016b69c50d236cc246c550b08377b
rajendrachy <chyrajendra32@gmail.com>
2025-08-10 23:45:28 +0530

    add assignment 1

--- CLASSNOTE/MODULAR_Assignment/app.js
+++ CLASSNOTE/MODULAR_Assignment/app.js
@@ -1,13 +1,36 @@
+// const express = require('express');
+// const app = express();
+// const userRoute = require('./routes/users');
+
+// app.use('/', userRoute);
+// // app.use('/user', userRoute);
+
+
+// app.listen(3000, () => {
+//   console.log("Server started");
+// })
+
+
+
+
+
+
+
+
+//----------another practice---------
 const express = require('express');
-const app = express();
+const path = require('path');
 const userRoute = require('./routes/users');
 
-app.use('/', userRoute);
-// app.use('/user', userRoute);
 
+const app = express();
+
+app.use('/', userRoute);
 
 app.listen(3000, () => {
-  console.log("Server started");
+  console.log("Server started on http://localhost:3000");
 })
+
+
 
 
--- CLASSNOTE/MODULAR_Assignment/routes/users.js
+++ CLASSNOTE/MODULAR_Assignment/routes/users.js
@@ -1,39 +1,103 @@
+// const express = require('express');
+// const path = require('path');
+// const fs = require('fs');
+
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.send("Hello !")
+// })
+
+
+// router.get('/user', (req, res)=> {
+//   res.sendFile(path.join(__dirname , '../dummy.json'));
+// })
+
+
+
+// router.get('/user/:id', (req, res) => {
+//     const id = req.params.id;
+//     console.log(id);
+
+//     fs.readFile(__dirname + '/../dummy.json', 'utf-8', (err1, data1) => {
+//             console.log(data1);
+//             const item = JSON.parse(data1);
+//             console.log(item);
+
+//             item.forEach((val) => {
+//                 if(val.id == id) {
+//                   console.log(val.user);
+//                   res.send(val.user);
+//                 }
+//             })
+//     })
+
+
+// })
+// module.exports = router;
+
+
+
+
+
+
+
+
+//---------Another practice------------------
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const dummyArr = require('../dummy.json');
+
 
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
-  res.send("Hello !")
+       res.send("Hello !");
 })
 
 
-router.get('/user', (req, res)=> {
-  res.sendFile(path.join(__dirname , '../dummy.json'));
+router.get('/user', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dummy.json'));
+  console.log(dummyArr);
+
+  const User = dummyArr.filter(val => val.user);
+console.log("The user is: ", User);
+   res.send(User);
 })
 
 
 router.get('/user/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
+  const id = req.params.id;
+  console.log("The id is: ", id);
 
-    fs.readFile(__dirname + '/../dummy.json', 'utf-8', (err1, data1) => {
-            console.log(data1);
-            const item = JSON.parse(data1);
-            console.log(item);
 
-            item.forEach((val) => {
-                if(val.id == id) {
-                  console.log(val.user);
-                  res.send(val.user);
-                }
-            })
-    })
+  fs.readFile(path.join(__dirname, '../dummy.json'), 'utf-8', (err, data) => {
+    if(!err) {
+      console.log(data);
 
+      const item = JSON.parse(data);
+      console.log("The item is: ", item);
+
+      const result = item.filter(val => val.id == id);
+      console.log("The result is: ", result);
+
+
+const plainText = result
+  .map(obj => `user: ${obj.user}, id: ${obj.id}`).join('\n');
+ 
+
+console.log(plainText);
+res.send(plainText);
+
+    }
+  })
 
 })
+
 module.exports = router;
+
 
 
 
