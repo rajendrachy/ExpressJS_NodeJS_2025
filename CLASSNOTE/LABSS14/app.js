4fa82cacc6669187c7261b889b31692573954487
rajendrachy <chyrajendra32@gmail.com>
2025-08-11 00:36:40 +0530

    labss 14 complete

--- CLASSNOTE/LABSS14/app.js
+++ CLASSNOTE/LABSS14/app.js
@@ -1,11 +1,35 @@
+// const express = require('express');
+// const app = express();
+
+// const userRoute = require('./routes/user');
+
+// app.use('/', userRoute);  
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+
+
+
+
+
+
+
+
 const express = require('express');
+const path = require('path');
+const routerPath = require('./routes/user');
+
 const app = express();
 
-const userRoute = require('./routes/user');
 
-app.use('/', userRoute);  
+app.use('/', routerPath);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+})
+
+
--- CLASSNOTE/LABSS14/routes/user.js
+++ CLASSNOTE/LABSS14/routes/user.js
@@ -1,5 +1,40 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const users = [
+//   { id: '101', name: 'Alice', age: 25 },
+//   { id: '102', name: 'Bob', age: 30 },
+//   { id: '103', name: 'Charlie', age: 22 }
+// ];
+
+// router.get('/user/:id', (req, res) => {
+//   const userId = req.params.id;
+
+//   const user = users.find(u => u.id === userId);
+
+//   if (user) {
+//     res.json(user); 
+//   } else {
+//     res.status(404).json({ message: "User not found" });
+//   }
+// });
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
 const express = require('express');
 const router = express.Router();
+
 
 const users = [
   { id: '101', name: 'Alice', age: 25 },
@@ -7,20 +42,34 @@ const users = [
   { id: '103', name: 'Charlie', age: 22 }
 ];
 
+
+
+router.get('/user', (req, res) => {
+  res.send(users);
+  console.log(users);
+})
+
+
 router.get('/user/:id', (req, res) => {
-  const userId = req.params.id;
+  const id = req.params.id;
+  console.log("User ID:", id);
 
-  const user = users.find(u => u.id === userId);
 
-  if (user) {
-    res.json(user); 
-  } else {
+  
+
+
+  const user = users.find(u => u.id === id);
+  console.log("Filtered User:", user);
+if(user) {
+  res.send(JSON.stringify(user));
+
+  } 
+
+
+  if(!user || user.length === 0) {
     res.status(404).json({ message: "User not found" });
   }
-});
+  
+})
 
 module.exports = router;
-
-
-
-
