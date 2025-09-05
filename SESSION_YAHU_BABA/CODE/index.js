f73e01514fa94bf5ab62aaaf12400db8be77fd0f
rajendrachy <chyrajendra32@gmail.com>
2025-09-05 11:49:14 +0530

    session note addes

---
+++ SESSION_YAHU_BABA/CODE/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const app = express();
+
+const session = require('express-session');
+
+
+//---------session middleware----------
+app.use(session({
+  secret: 'secretpassword',
+  resave:false,
+  saveinitialized:false,
+  cookie: {maxAge: 1000*60 * 60 * 24} // delete after 1 day or 24 hour automatically
+}))
+
+app.get("/", (req, res) => {
+    if(req.session.username) {
+    res.send(`Username from session is: ${req.session.username}`)
+  } else {
+      res.send('No username found in session');
+
+  }
+})
+
+
+//--------creating session--------------
+app.get('/set-username', (req, res) => {
+  req.session.username = "rc"; // username-> variable we can choose any 
+  res.send('user name has been set in session');
+})
+
+
+//------reading session--------------
+app.get('/get-username', (req, res) => {
+  if(req.session.username) {
+    res.send(`Username from session is: ${req.session.username}`)
+  } else {
+      res.send('No username found in session');
+
+  }
+})
+
+
+
+
+app.get('/destroy', (req, res) => {
+   req.session.destroy((err) => {
+     if(err) {
+    res.status(500).send("failed to destroy session")
+     }
+
+    res.status(200).send("session destroy successfully")
+
+
+   });
+})
+
+app.listen(3000, () => {
+  console.log("Server started");
+})
