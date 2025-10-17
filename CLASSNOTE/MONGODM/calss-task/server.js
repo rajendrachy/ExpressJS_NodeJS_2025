de00d151046ebe8ba68db8bd2af0f41146e4b61e
rajendrachy <chyrajendra32@gmail.com>
2025-10-17 09:29:03 +0530

    ad

---
+++ CLASSNOTE/MONGODM/calss-task/server.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const ejs = require("ejs")
+const {ObjectId} = require("mongodb")
+const {connectMongo} = require("./utils/connectDb")
+
+const app = express()
+
+app.use(express.urlencoded())
+
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+
+
+
+let db;
+
+connectMongo()
+.then(data => db = data)
+
+
+
+
+app.get("/", async (req, res)=>{
+    try {
+        let data = await db.collection("students").find().toArray();
+        res.render("dashboard", {students: data})
+    } catch (error) {
+        
+    }
+})
+
+
+
+
+
+
+app.post("/add-student", async (req, res)=>{
+    try {
+        const{name, section,marks} = req.body;
+
+       await db.collection("students").insertOne({name, section, marks: Number(marks)})
+       res.redirect("/")
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.post("/filter", async(req, res)=>{
+    try {
+       
+        let data = await db.collection("students").find(
+            {marks: {$gt : Number(req.body.minMarks)}}).toArray()
+        res.render("dashboard", {students: data})
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+
+
+
+
+app.delete("/delete/:id", async (req, res)=>{
+    console.log(req.params.id)
+    await db.collection("students").deleteOne({_id : new ObjectId(req.params.id)})
+    res.redirect("/")
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('server started..')
+})
+
+
+
+
+
+
