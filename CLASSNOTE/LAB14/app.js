730d14becc68d5219e7a6898c2d9aed61d712e57
rajendrachy <chyrajendra32@gmail.com>
2025-12-11 10:36:20 +0530

    la14

--- CLASSNOTE/LAB14/app.js
+++ CLASSNOTE/LAB14/app.js
@@ -1,14 +1,38 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+
+// const greetRoute = require('./routes/greet');
+
+// app.use('/', greetRoute);
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
+
+
+
+
+
+
+
+
+
+const express = require("express");
+const fs = require('fs');
+
+const greeting = require("./routes/greet")
+
 const app = express();
 
-const greetRoute = require('./routes/greet');
+app.use('/', greeting);
 
-app.use('/', greetRoute);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(3000, () => {
+  console.log("Server Started");
+})
+
 
 
 
--- CLASSNOTE/LAB14/routes/greet.js
+++ CLASSNOTE/LAB14/routes/greet.js
@@ -1,45 +1,81 @@
+// const express = require('express');
+// const router = express.Router();
+
+
+// router.get('/', (req, res) => {
+//    res.send("Home page of Greeting");
+
+// })
+
+
+
+// router.get('/gree', (req, res) => {
+//   const lan = req.query.lan;
+//   console.log("first", lan);
+//   let message;
+
+
+//   switch (lan) {
+//     case 'en':
+//       message = 'Hello';
+//       //console.log("second", lang.message);
+//       break;
+//     case 'fr':
+//       message = 'Bonjour';
+//       break;
+//     case 'hi':
+//       message = 'Namaste';
+//       break;
+//     default:
+//       message = 'Hello (Default)';
+//   }
+
+//   res.send(message);
+// });
+
+
+// //module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
 const express = require('express');
+
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-   res.send("Home page of Greeting");
 
+router.get('/greet', (req, res) => {
+      const lan = req.query.lan;
+
+      console.log("first", lan);
+
+      let msg = "";
+
+      if(lan == 'eng') {
+        msg = "Hello";
+      } else if(lan == 'fr') {
+        msg = "ngjdfjgndf";
+      }
+
+      res.send(msg);
 })
 
 
 
-router.get('/gree', (req, res) => {
-  const lan = req.query.lan;
-  console.log("first", lan);
-  let message;
 
 
-  switch (lan) {
-    case 'en':
-      message = 'Hello';
-      //console.log("second", lang.message);
-      break;
-    case 'fr':
-      message = 'Bonjour';
-      break;
-    case 'hi':
-      message = 'Namaste';
-      break;
-    default:
-      message = 'Hello (Default)';
-  }
 
-  res.send(message);
-});
 
 
 module.exports = router;
-
-
-
-
-
-
-
 
